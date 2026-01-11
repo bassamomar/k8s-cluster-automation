aac6a752cfad225c9034ab2dcc4f7b0e123cc86a
Bassam Omar <omar.bassam@pivotaccess.com>
2026-01-11 13:42:24 +0200

    chore: provide sane default for variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,13 @@
 variable "cluster_name" {
   description = "A name to provide for the Talos cluster"
   type        = string
+  default     = "k8s"
 }
 
 variable "iso_path" {
   description = "Path to the Talos ISO"
   type        = string
+  default     = "/metal-amd64.iso"
 }
 
 variable "cluster_size" {
